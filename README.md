9835d26bb7acdbb14781fa47027cfee4e6653b94
d1y <chenhonzhou@gmail.com>
2021-07-24 11:25:44 +0800

    fix: preview error

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # icns_preview
 
+![](https://badgen.net/vs-marketplace/v/waifuproject.icns-preview)
+
 ![](./preview.png)
 
 vscode support [icns](https://en.wikipedia.org/wiki/Apple_Icon_Image_format#Icon_types) file preview
+
+![](https://files.catbox.moe/j9o0xi.gif)
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,48 @@
 import * as vscode from 'vscode'
-import { readIcns } from './icns'
+import { icnsApp, readIcns } from './icns'
 import { FormatError, Title, Wrap } from './msg'
+
+const renderHTML = (incs: icnsApp)=> {
+	let options = ``
+	let firstImage = ``
+	incs.images.forEach((item, index)=> {
+		const type = item.osType
+		const image = incs.toImage(item.image)
+		if (index == 0) firstImage = image
+		options += `<option value="${ image }">${ type }</option>`
+		return {
+			type,
+			image
+		}
+	})
+	return `
+		<style>
+			#app select {
+				border-radius: 4px;
+				width: 20vw;
+				padding: 4px 12px;
+				min-width: 120px;
+				max-width: 420px;
+				margin: 12px 0;
+				outline: none;
+			}
+		</style>
+		<div id="app" class="select">
+			<select onChange="window.changeShowImg(this.value)" name="" id="">
+				${ options }
+			</select>
+			<div>
+				<img id="img" src="${ firstImage }" />
+			</div>
+		</div>
+		<script type="text/javascript">
+			const img = document.getElementById('img')
+			window.changeShowImg = function (val) {
+				img.setAttribute("src", val)
+			}
+		</script>
+	`
+}
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -15,15 +57,11 @@ export function activate(context: vscode.ExtensionContext) {
 			return
 		}
 
-		const pan =	vscode.window.createWebviewPanel(Title, Title, vscode.ViewColumn.Active)
+		const pan =	vscode.window.createWebviewPanel(Title, Title, vscode.ViewColumn.Active, {
+			enableScripts: true,
+		})
 
-		let index = 4
-
-		if (next.images.length <= 4) index = 0
-
-		const img = next.toImage(next.images[index].image)
-
-		pan.webview.html = `<div><img src="${ img }" /></div>`
+		pan.webview.html = renderHTML(next)
 
 	})
 
